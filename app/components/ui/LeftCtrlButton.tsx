9c65d78fccc6bcd22464967c70b4ee250924ddd5
jaybo1001 <138844854+jaybo1001@users.noreply.github.com>
2024-05-24 11:10:09 -0700

    leftbutton works

---
+++ app/components/ui/LeftCtrlButton.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import * as React from "react";
+import * as HoverCardPrimitive from "@radix-ui/react-hover-card";
+import { cn } from "@/lib/utils";
+import { CalendarDays } from "lucide-react";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+
+const HoverCard = HoverCardPrimitive.Root;
+const HoverCardTrigger = HoverCardPrimitive.Trigger;
+
+const HoverCardContent = React.forwardRef<
+  React.ElementRef<typeof HoverCardPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof HoverCardPrimitive.Content>
+>(({ className, align = "center", side = "right", sideOffset = 4, ...props }, ref) => (
+  <HoverCardPrimitive.Content
+    ref={ref}
+    align={align}
+    side={side}
+    sideOffset={sideOffset}
+    className={cn(
+      "z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+      className
+    )}
+    {...props}
+  />
+));
+HoverCardContent.displayName = HoverCardPrimitive.Content.displayName;
+
+export { HoverCard, HoverCardTrigger, HoverCardContent };
+
+interface LeftCtrlButtonProps {
+  imageSrc: string;
+  hoverText: string;
+}
+
+const LeftCtrlButton: React.FC<LeftCtrlButtonProps> = ({ imageSrc, hoverText }) => {
+  return (
+    <HoverCard>
+      <HoverCardTrigger asChild>
+        <div className="left-ctrl-button">
+          <img
+            src={imageSrc}
+            alt={hoverText}
+            className="left-ctrl-button-img"
+          />
+        </div>
+      </HoverCardTrigger>
+      <HoverCardContent className="w-80">
+        <div className="flex justify-between space-x-4">
+          <Button className="large-button">
+            <img src={imageSrc} alt={hoverText} className="large-button-img" />
+          </Button>
+          <div className="space-y-1">
+            <h4 className="text-sm font-semibold popup-text">@nextjs</h4>
+            <p className="text-sm popup-text">
+              {hoverText} {/* Use the hoverText here */}
+            </p>
+            <div className="flex items-center pt-2">
+              <CalendarDays className="mr-2 h-4 w-4 opacity-70" />
+              <span className="text-xs popup-text-muted">
+                Joined December 2021
+              </span>
+            </div>
+          </div>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
+  );
+};
+
+export default LeftCtrlButton;
--- app/layout.jsx
+++ app/layout.jsx
@@ -6,118 +6,59 @@
 'use client';
 import './globals.css';
 import Draggable from 'react-draggable';
-import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar"
 import CleoChatComponent from '@/components/CleoChatComponent';
+import { Avatar } from "@/components/ui/avatar";
+import LeftCtrlButton from '@/components/ui/LeftCtrlButton';
 
 export default function Layout() {
   return (
     <html lang="en">
       <body>
-    <div
-      key="1"
-      className="border-4 border-[#FFFF00] rounded-lg p-1 bg-gradient-to-b from-[#000000] to-[#217CAF] min-h-screen flex flex-row relative pt-20 pb-2 pl-3 pr-3"
-    >
-      <div className="absolute top-0 left-0 w-full text-white py-2 px-4 z-10 flex justify-between items-center">
-      <img
-        alt="Logo"
-        className="object-contain"
-        src="/images/neucleos_logo.png"
-        style={{
-          transform: "scale(0.4)",
-          transformOrigin: "top left", // Adjust the origin as needed
-        }}
-      />
-        <div className="mx-auto">
-        </div>
-        <Avatar>
-          <AvatarImage alt="@shadcn" src="/placeholder-avatar.jpg" />
-          <AvatarFallback className="ml-auto">CN</AvatarFallback>
-        </Avatar>
-      </div>
-      <div className="flex flex-col w-[40px] mr-0 font-montserrat">
-        <div className="flex flex-col bg-[#000000] mt-8 text-white h-[40px] rounded-tl-lg rounded-bl-lg rounded-tr-none rounded-br-none border-2 border-[#12b656] p-4 items-center justify-center ml-auto w-full"
-        style={{
-          borderRight: 'none',
-        }} />
-        <div className="flex flex-col bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-bl-lg rounded-tr-none rounded-br-none border-2 border-[#12b656] p-4 items-center justify-center ml-auto w-full"         
-        style={{
-          borderRight: 'none',
-        }} />
-         <div className="flex flex-col bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-bl-lg rounded-tr-none rounded-br-none border-2 border-[#12b656] p-4 items-center justify-center ml-auto w-full"         
-        style={{
-          borderRight: 'none',
-        }} />
-         <div className="flex flex-col bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-bl-lg rounded-tr-none rounded-br-none border-2 border-[#12b656] p-4 items-center justify-center ml-auto w-full"         
-        style={{
-          borderRight: 'none',
-        }} />
-         <div className="flex flex-col bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-bl-lg rounded-tr-none rounded-br-none border-2 border-[#12b656] p-4 items-center justify-center ml-auto w-full"         
-        style={{
-          borderRight: 'none',
-        }} />
-         <div className="flex flex-col bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-bl-lg rounded-tr-none rounded-br-none border-2 border-[#12b656] p-4 items-center justify-center ml-auto w-full"         
-        style={{
-          borderRight: 'none',
-        }} />
-         <div className="flex flex-col bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-bl-lg rounded-tr-none rounded-br-none border-2 border-[#12b656] p-4 items-center justify-center ml-auto w-full"         
-        style={{
-          borderRight: 'none',
-        }} />
-         <div className="flex flex-col bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-bl-lg rounded-tr-none rounded-br-none border-2 border-[#12b656] p-4 items-center justify-center ml-auto w-full"         
-        style={{
-          borderRight: 'none',
-        }} />
-        <div className="flex flex-col bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-bl-lg rounded-tr-none rounded-br-none border-2 border-[#12b656] p-4 items-center justify-center ml-auto w-full" 
-        style={{
-          borderRight: 'none',
-        }} />
-
-      </div>
-      <div className="flex w-[400px] mx-auto rounded-lg flex-col">
-{/*Cleo Container*/}
-      <div className="bg-[#000000] max-h-[100vh] overflow-auto text-white flex-1 rounded-tl-lg rounded-bl-lg rounded-tr-lg rounded-br-none border-4 border-[#FF6A00] mt-[0px] p-4 flex flex-col justify-between font-montserrat mt-50">
-        <CleoChatComponent />
-        </div>
-      </div>
-      <div className="flex flex-col w-full">
-        <div className="flex gap-0">
-          <div className="bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-tr-lg rounded-bl-none rounded-br-none p-2 flex items-center flex-1 font-montserrat border-4 border-green-500" />
-          <div className="bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-tr-lg rounded-bl-none rounded-br-none p-2 flex items-center flex-1 font-montserrat" />
-          <div className="bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-tr-lg rounded-bl-none rounded-br-none p-2 flex items-center flex-1 font-montserrat" />
-          <div className="bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-tr-lg rounded-bl-none rounded-br-lg p-2 flex items-center w-[25px] font-montserrat" />
-        </div>
-        <div className="bg-[#000000] text-white flex-1 rounded-lg border-4 border-[#12b656] mt-[-8px] p-6 rounded-br-lg rounded-bl-none rounded-tl-none font-montserrat grid grid-cols-[100%]" />
-      </div>
-      <div className="flex flex-col w-[50px] mr-0 font-montserrat">
-        <div className="bg-[#000000] text-white h-[40px] flex items-center justify-center" />
-        <div className="flex flex-col bg-[#000000] text-white h-[40px] rounded-tl-none rounded-bl-none rounded-tr-lg rounded-br-lg border-2 border-[#ECA902] p-4 items-center justify-center ml-auto">
-          <img
-            alt="Icon"
-            className="icon"
-            height={45}
-            src="/placeholder.svg"
-            style={{
-              aspectRatio: "45/45",
-              objectFit: "cover",
-            }}
-            width={45}
-          />
-        </div>
-        <div>
-          <div className="flex flex-col bg-[#000000] text-white h-[40px] rounded-tl-none rounded-bl-none rounded-tr-lg rounded-br-lg border-2 border-[#ECA902] p-4 items-center justify-center ml-auto">
+        <div
+          key="1"
+          className="border-4 border-[#FFFF00] rounded-lg p-1 bg-gradient-to-b from-[#000000] to-[#217CAF] min-h-screen flex flex-row relative pt-20 pb-2 pl-3 pr-3"
+        >
+          <div className="absolute top-0 left-0 w-full text-white py-2 px-4 z-10 flex justify-between items-center">
             <img
-              alt="Icon"
-              className="icon"
-              height={45}
-              src="/placeholder.svg"
+              alt="Logo"
+              className="object-contain"
+              src="/images/neucleos_logo.png"
               style={{
-                aspectRatio: "45/45",
-                objectFit: "cover",
+                transform: "scale(0.4)",
+                transformOrigin: "top left",
               }}
-              width={45}
             />
+            <div className="mx-auto"></div>
+            <Avatar src="/images/avatar.png" alt="@shadcn" className="w-10 h-10" initials="CN" />
           </div>
-          <div>
+          <div className="flex flex-col w-[40px] mr-0 mt-[40px] font-montserrat">
+            <LeftCtrlButton imageSrc="/icons/icon1.png" hoverText="Description 1" />
+            <LeftCtrlButton imageSrc="/icons/icon2.png" hoverText="Description 2" />
+            <LeftCtrlButton imageSrc="/icons/icon3.png" hoverText="Description 3" />
+            <LeftCtrlButton imageSrc="/icons/icon4.png" hoverText="Description 4" />
+            <LeftCtrlButton imageSrc="/icons/icon5.png" hoverText="Description 5" />
+            <LeftCtrlButton imageSrc="/icons/icon6.png" hoverText="Description 6" />
+            <LeftCtrlButton imageSrc="/icons/icon7.png" hoverText="Description 7" />
+            <LeftCtrlButton imageSrc="/icons/icon8.png" hoverText="Description 8" />
+            <LeftCtrlButton imageSrc="/icons/icon9.png" hoverText="Description 9" />
+          </div>
+          <div className="flex w-[400px] mx-auto rounded-lg flex-col">
+            {/*Cleo Container*/}
+            <div className="bg-[#000000] max-h-[100vh] overflow-auto text-white flex-1 rounded-tl-lg rounded-bl-lg rounded-tr-lg rounded-br-none border-4 border-[#FF6A00] mt-[0px] p-4 flex flex-col justify-between font-montserrat mt-50">
+              <CleoChatComponent />
+            </div>
+          </div>
+          <div className="flex flex-col w-full">
+            <div className="flex gap-0">
+              <div className="bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-tr-lg rounded-bl-none rounded-br-none p-2 flex items-center flex-1 font-montserrat border-4 border-green-500" />
+              <div className="bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-tr-lg rounded-bl-none rounded-br-none p-2 flex items-center flex-1 font-montserrat" />
+              <div className="bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-tr-lg rounded-bl-none rounded-br-none p-2 flex items-center flex-1 font-montserrat" />
+              <div className="bg-[#000000] text-white h-[40px] rounded-tl-lg rounded-tr-lg rounded-bl-none rounded-br-lg p-2 flex items-center w-[25px] font-montserrat" />
+            </div>
+            <div className="bg-[#000000] text-white flex-1 rounded-lg border-4 border-[#12b656] mt-[-8px] p-6 rounded-br-lg rounded-bl-none rounded-tl-none font-montserrat grid grid-cols-[100%]" />
+          </div>
+          <div className="flex flex-col w-[50px] mr-0 font-montserrat">
+            <div className="bg-[#000000] text-white h-[40px] flex items-center justify-center" />
             <div className="flex flex-col bg-[#000000] text-white h-[40px] rounded-tl-none rounded-bl-none rounded-tr-lg rounded-br-lg border-2 border-[#ECA902] p-4 items-center justify-center ml-auto">
               <img
                 alt="Icon"
@@ -131,11 +72,39 @@ export default function Layout() {
                 width={45}
               />
             </div>
+            <div>
+              <div className="flex flex-col bg-[#000000] text-white h-[40px] rounded-tl-none rounded-bl-none rounded-tr-lg rounded-br-lg border-2 border-[#ECA902] p-4 items-center justify-center ml-auto">
+                <img
+                  alt="Icon"
+                  className="icon"
+                  height={45}
+                  src="/placeholder.svg"
+                  style={{
+                    aspectRatio: "45/45",
+                    objectFit: "cover",
+                  }}
+                  width={45}
+                />
+              </div>
+              <div>
+                <div className="flex flex-col bg-[#000000] text-white h-[40px] rounded-tl-none rounded-bl-none rounded-tr-lg rounded-br-lg border-2 border-[#ECA902] p-4 items-center justify-center ml-auto">
+                  <img
+                    alt="Icon"
+                    className="icon"
+                    height={45}
+                    src="/placeholder.svg"
+                    style={{
+                      aspectRatio: "45/45",
+                      objectFit: "cover",
+                    }}
+                    width={45}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    </body>
-</html>
-  )
+      </body>
+    </html>
+  );
 }
